2538d8bf9c56b317c934766489bc1d4b9edcdc2a
Geert Van Bastelaere <geert.van.bastelaere@gmail.com>
2020-06-07 20:29:19 +0200

    Spring eventing bridge

---
+++ spring-events/src/test/java/test/camunda/MyListener.java
@@ -0,0 +1,48 @@
+package test.camunda;
+
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.impl.history.event.HistoryEvent;
+import org.camunda.bpm.spring.boot.starter.event.ExecutionEvent;
+import org.camunda.bpm.spring.boot.starter.event.TaskEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
+import static org.apache.commons.lang3.builder.ToStringStyle.JSON_STYLE;
+
+@Slf4j
+@Component
+public class MyListener {
+
+    protected void log(String message, Object value) {
+        log.info("{}: {} - {}", message, value.getClass().getName(), reflectionToString(value, JSON_STYLE));
+    }
+
+    @EventListener
+    public void onDelegateTask(DelegateTask delegateTask) {
+        log("onDelegateTask", delegateTask);
+    }
+
+    @EventListener
+    public void onTaskEvent(TaskEvent taskEvent) {
+        log("onTaskEvent", taskEvent);
+    }
+
+    @EventListener
+    public void onDelegateExecution(DelegateExecution delegateExecution) {
+        log("onDelegateExecution", delegateExecution);
+    }
+
+    @EventListener
+    public void onExecutionEvent(ExecutionEvent executionEvent) {
+        log("onExecutionEvent", executionEvent);
+    }
+
+    @EventListener
+    public void onHistoryEvent(HistoryEvent historyEvent) {
+        log("onHistoryEvent", historyEvent);
+    }
+
+}
